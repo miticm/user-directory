08de8741987a815d3593996eeb1570f24bde394f
Tianchi Xu <xtc459924276@gmail.com>
2018-05-15 14:10:08 -0400

    change the second heading when submit form

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const headings = document.querySelectorAll('h1')
-
-for(let i = 0; i < headings.length;i++){
-    headings[i].addEventListener('click',function changeText() {
-        headings[i].textContent = 'Bye'
-    })
-}
+const btn = document.querySelector('button')
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+// for(let i = 0; i < headings.length;i++){
+//     headings[i].addEventListener('click',function changeText() {
+//         headings[i].textContent = 'Bye'
+//     })
+// }
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    headings[1].textContent = input.value
+    form.reset()
+})
