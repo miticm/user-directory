fa667219fbe43b6e2d6f5548109d30e57ebdbc0e
Tianchi Xu <xtc459924276@gmail.com>
2018-05-15 14:30:31 -0400

    add README.md

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const headings = document.querySelectorAll('h1')
-const btn = document.querySelector('button')
 const form = document.querySelector('form')
-const input = document.querySelector('input')
-// for(let i = 0; i < headings.length;i++){
-//     headings[i].addEventListener('click',function changeText() {
-//         headings[i].textContent = 'Bye'
-//     })
-// }
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    headings[1].textContent = input.value
+    headings[1].textContent = form.change.value
     form.reset()
 })
