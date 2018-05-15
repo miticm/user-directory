5fa31edc23939dc5abfe792b0c7354c6fd383617
Tianchi Xu <xtc459924276@gmail.com>
2018-05-15 15:30:39 -0400

    append the user as a proper DOM element

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const handleSubmit = function(ev) {
   const f = ev.target
   const userName = f.userName.value
   const age = f.age.value
-  users.innerHTML += `<p>${userName}, ${age}</p>`
+  //users.innerHTML += `<p>${userName}, ${age}</p>`
+
+  const p = document.createElement('p')   
+  p.textContent = `${userName}, ${age}`
+
+  users.appendChild(p)
+
   f.reset()
   f.userName.focus()
 }
