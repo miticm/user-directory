c70298ddcd8b76ba32daf148b6dadd8ca458771c
Tianchi Xu <xtc459924276@gmail.com>
2018-05-14 20:06:36 -0400

    change the heading text when keyup event is fired in the input

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const form = document.querySelector('form')
-
 const heading = document.querySelector('h1')
 
-const input = document.querySelector('input')
+const inputTextfield = document.querySelector('input')
 
 function changeText(ev) {
-    ev.preventDefault();
-    heading.textContent = input.value
-    form.reset();
+    heading.textContent = inputTextfield.value
 }
 
-form.addEventListener('submit', changeText)
+inputTextfield.addEventListener('keyup', changeText)
